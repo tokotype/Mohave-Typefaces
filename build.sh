8849877d4c232e9437e8ed1629de87ee54d4d9fe
mirko velimirovic <bghryct@gmail.com>
2019-11-21 18:28:11 -0500

    fixed vf build problem in roman

--- build.sh
+++ build.sh
@@ -28,38 +28,40 @@ set -e
 
 echo "Generating VFs"
 # mkdir -p ./fonts/variable
-fontmake -g source/Mohave.glyphs -o variable --round-instances --output-path ./fonts/vf/Mohave\[wght\].ttf
-fontmake -g source/Mohave-Italic.glyphs -o variable --round-instances --output-path ./fonts/vf/Mohave-Italic\[wght\].ttf
+fontmake -g sources/Mohave.glyphs -o variable --round-instances --output-path ./fonts/vf/Mohave\[wght\].ttf
+# fontmake -g sources/Mohave-Italic.glyphs -o variable --round-instances --output-path ./fonts/vf/Mohave-Italic\[wght\].ttf
 
-echo "Removing Build UFOS"
+# echo "Removing Build UFOS"
 
-rm -rf master_ufo/ instance_ufo/
+# rm -rf master_ufo/ instance_ufo/
 
-echo "Build UFOS Removed"
+# echo "Build UFOS Removed"
 
-echo "Post processing"
+# echo "Post processing"
 
-vfs=$(ls ./fonts/vf/*.ttf)
-for vf in $vfs
-do
-	gftools fix-dsig -f $vf;
-	gftools fix-nonhinting $vf "$vf.fix";
-	mv "$vf.fix" $vf;
-	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
-	rtrip=$(basename -s .ttf $vf)
-	new_file=./fonts/vf/$rtrip.ttx;
-	rm $vf;
-	ttx $new_file
-	rm ./fonts/vf/*.ttx
-done
-rm ./fonts/vf/*backup*.ttf
+# vfs=$(ls ./fonts/vf/*.ttf)
+# for vf in $vfs
+# do
+# 	gftools fix-dsig -f $vf;
+# 	gftools fix-nonhinting $vf "$vf.fix";
+# 	mv "$vf.fix" $vf;
+# 	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+# 	rtrip=$(basename -s .ttf $vf)
+# 	new_file=./fonts/vf/$rtrip.ttx;
+# 	rm $vf;
+# 	ttx $new_file
+# 	rm ./fonts/vf/*.ttx
+# done
+# rm ./fonts/vf/*backup*.ttf
 
-echo "fix vf meta? ok let's try that. I'm trying to fix VF meta"
-gftools fix-vf-meta $vfs;
-for vf in $vfs
-do
-	mv "$vf.fix" $vf;
-done
+# echo "fix vf meta? ok let's try that. I'm trying to fix VF meta"
+# gftools fix-vf-meta $vfs;
+# for vf in $vfs
+# do
+# 	mv "$vf.fix" $vf;
+# done
 
-echo "Post processing complete"
+# echo "Post processing complete"
+
+# fontbakery check-googlefonts fonts/vf/*.ttf --ghmarkdown MohaveChecks.md 
 
